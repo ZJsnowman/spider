77d052a9bdf98a1031508164317b5b3676922730
ZJsnowman <zj2299490@gmail.com>
2018-04-14 11:50:48 +0800

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-# execute(['scrapy', 'crawl', 'jobbole'])
-# execute(['scrapy', 'crawl', 'zhihu'])
+# # execute(['scrapy', 'crawl', 'jobbole'])
+# # execute(['scrapy', 'crawl', 'zhihu'])
 execute(['scrapy', 'crawl', 'tianyancha'])
-# execute(['scrapy', 'crawl', 'errback_example'])
+
